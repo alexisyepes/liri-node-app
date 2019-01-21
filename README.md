bcee9d056b51322acfcc3af0bc3def6c4c58577b
Alex Yepes <alexisyepes@yahoo.com>
2019-01-21 17:54:24 -0500

    Final commit for project

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # liri-node-app
+
+Object
+
+There is no HTML page, so I've used screenshots and a video below to explain how it works. Liri has 4 commands:
+
+1) concert-this uses the Request NPM from Bands in town to get information about concerts/location/dates
+2) spotify-this-song uses the Spotify NPM to pull information from the returned object based on the song the user is looking for.
+3) movie-this uses the Request NPM to pull data from the OMDB database for the user's search.
+4) The do-what-it-says command uses fs.write to write to the random.txt file in the directory.
+
+
+I used a switch with "cases" and corresponding functions to create the four separate commands:
+
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+    default: console.log("\n" + "please type any command after: node liri.js: " + "\n" +
+        "concert-this  any artist/band name " + "\n" +
+        "spotify-this-song  any song title " + "\n" +
+        "movie-this  any movie title " + "\n" +
+        "do-what-it-says  " + "\n"
+};
+
+![Image 1](./images/img1.png)
+
+Instrucions
+
+For LIRI to execute your orders, you need to write: node liri.js followed by one of the 4 commands (see example below). 
+
+concert-this  
+spotify-this-song
+movie-this
+do-what-it-says
+
+
+
+![Image 2](./images/img2.png)
+
+
+Then, write your corresponding search and press Enter.
+
+You will see the results like this:
+
+![Image 3](./images/img3.png)
+
+
+Examples:
+
+node liri.js concert-this "Metallica"
+node liri.js spotify-this-song "Fear of the dark"
+node liri.js movie-this "The rock"
+node liri.js do-what-it-says
+
+*Don't forget to write your search inside single or double quotes*
+
+A small video shows the demo in action!
+please follow the link below
+
+https://drive.google.com/file/d/1pMo0lbMSJNJ2_ml8J4x8AN-Ub89XMLDP/view
+
---
+++ liri.js
@@ -0,0 +1,146 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var fs = require('fs');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var request = require("request");
+var movieName = process.argv[3];
+var liriReturn = process.argv[2];
+var moment = require('moment');
+
+switch (liriReturn) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+    default: console.log("\n" + "please type any command after: node liri.js: " + "\n" +
+        "concert-this  any artist/band name " + "\n" +
+        "spotify-this-song  any song title " + "\n" +
+        "movie-this  any movie title " + "\n" +
+        "do-what-it-says  " + "\n"
+    );
+
+};
+
+function concertThis() {  
+
+    var artist = process.argv[3];
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+        request(queryUrl, function (error, response, data) {
+
+            if (!error && response.statusCode === 200) {
+                var bandReturn = JSON.parse(data)
+
+                for (let i = 0; i < bandReturn.length; i++) {
+                    var event = bandReturn[i]
+                    console.log("**********************LIRI FOUND THESE RESULTS FOR YOU**********************" + 
+                    "\n" + artist + "\nVenue: " + event.venue.name + "\nCity: " + event.venue.city + "\nDate: " + moment(event.datetime).format("MM/DD/YYYY"))
+                }
+            }
+        });
+
+       
+};
+
+function spotifyThisSong(songName) {
+    var songName = process.argv[3];
+    if (!songName) {
+        songName = "The Sign";
+    };
+    songRequest = songName;
+    spotify.search({
+        type: "track",
+        query: songRequest
+    },
+        function (err, data) {
+            if (!err) {
+                var trackInfo = data.tracks.items;
+                for (var i = 0; i < 5; i++) {
+                    if (trackInfo[i] != undefined) {
+                        var spotifyResults =
+                            "Artist: " + trackInfo[i].artists[0].name + "\n" +
+                            "Song: " + trackInfo[i].name + "\n" +
+                            "Preview URL: " + trackInfo[i].preview_url + "\n" +
+                            "Album: " + trackInfo[i].album.name + "\n"
+
+                        console.log("**********************LIRI FOUND THESE RESULTS FOR YOU**********************" + "\n" +
+                        + "\n" + spotifyResults);
+                        console.log(' ');
+                    };
+                };
+            } else {
+                console.log("error: " + err);
+                return;
+            };
+        });
+};
+
+function movieThis() {
+
+    if (!movieName) {
+        movieName = "Mr Nobody"
+    };
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=e2bed0cc";
+
+
+    request(queryUrl, function (error, response, body) {
+
+
+
+        if (!error && response.statusCode === 200) {
+
+            var myMovieData = JSON.parse(body);
+            var queryUrlResults =
+                "Title: " + myMovieData.Title + "\n" +
+                "Year: " + myMovieData.Year + "\n" +
+                "IMDB Rating: " + myMovieData.Ratings[0].Value + "\n" +
+                "Rotten Tomatoes Rating: " + myMovieData.Ratings[1].Value + "\n" +
+                "Origin Country: " + myMovieData.Country + "\n" +
+                "Language: " + myMovieData.Language + "\n" +
+                "Plot: " + myMovieData.Plot + "\n" +
+                "Actors: " + myMovieData.Actors + "\n"
+
+            console.log("**********************LIRI FOUND THESE RESULTS FOR YOU**********************" + "\n" +
+            queryUrlResults);
+        } else {
+            console.log("error: " + err);
+            return;
+        };
+    });
+};
+
+function doWhatItSays() {
+
+    fs.writeFile("random.txt", 'spotify-this-song,"I want it this way"', function (err) {
+        var song = "spotify-this-song 'I want it this way'"
+
+        if (err) {
+            return console.log(err);
+        };
+
+        console.log(song);
+    });
+};
+
+
+
+
+
+
+
+
